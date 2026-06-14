72a963c3c686f70612d7853e337b3f7c95c93d0e
GVonFlue <gvonflue@gmail.com>
2026-06-14 13:50:28 -0500

    Update page.jsx

--- app/jointheflock/thanks/page.jsx
+++ app/jointheflock/thanks/page.jsx
@@ -46,7 +46,7 @@ export default function JoinTheFlockThanks() {
         @media (max-width: 900px) { .thanks-sparkle { display: none; } }
       `}</style>
 
-      <div style={topBarStyle}><Lockup /></div>
+      <DuckNav />
 
       <section style={wrapStyle}>
         <svg className="thanks-sparkle s1" width="28" height="28" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 0 L13.5 10.5 L24 12 L13.5 13.5 L12 24 L10.5 13.5 L0 12 L10.5 10.5 Z" fill="var(--gold)"/></svg>
