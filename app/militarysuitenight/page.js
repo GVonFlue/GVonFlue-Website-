63f461f0344430b4b84827ec447f6d7b1f6b23cc
GVonFlue <gvonflue@gmail.com>
2026-08-02 20:37:03 -0500

    Update page.js

--- app/militarysuitenight/page.js
+++ app/militarysuitenight/page.js
@@ -18,7 +18,7 @@ const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwmN-Ay_a19_I5q
 const DM_URL = "https://instagram.com/gvonflue";
 
 /* Sponsor-a-Veteran fund */
-const STRIPE_LINK = "https://buy.stripe.com/PASTE_YOUR_LINK"; // your Stripe Payment Link
+const STRIPE_LINK = "https://buy.stripe.com/9B6cN614Z5oS6sMfOVdQQ01"; // your Stripe Payment Link
 const FUND_GOAL = 250;   // stadium-fill goal, number of vets
 const FUND_PRICE = 12;   // dollars per veteran
 
